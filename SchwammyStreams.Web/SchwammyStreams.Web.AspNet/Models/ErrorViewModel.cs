22bcb488fe1165f7e28e7e8ae31c90911437e407
Andy Schwam <andys987@hotmail.com>
2020-01-01 18:59:14 -0500

    initial commit of projects

---
+++ SchwammyStreams.Web/SchwammyStreams.Web.AspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchwammyStreams.Web.AspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
