b14690cf59c74d87177ea96cfc3700a0d9e6de21
Andy Schwam <andys987@hotmail.com>
2020-01-07 19:26:20 -0500

    very minimal content for home page

--- SchwammyStreams.Web/SchwammyStreams.Web.AspNet/Startup.cs
+++ SchwammyStreams.Web/SchwammyStreams.Web.AspNet/Startup.cs
@@ -32,6 +32,7 @@ namespace SchwammyStreams.Web.AspNet
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //app.UseBrowserLink();
             }
             else
             {
