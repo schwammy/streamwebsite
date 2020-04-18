6141d316e13af4597498e16d6b4ea528f83d221d
Andy Schwam <andys987@hotmail.com>
2020-04-17 23:21:03 -0400

    sql auth stuff

---
+++ SchwammyStreams.Web/SchwammyStreams.Db/Script.PostDeployment1.sql
@@ -0,0 +1,24 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+if not exists(select * from sys.database_principals where name = '[schwammystreamsweb-svc-systest]')
+BEGIN
+
+create user [schwammystreamsweb-svc-systest] from external provider;
+
+ALTER ROLE db_datareader
+  ADD MEMBER [schwammystreamsweb-svc-systest] 
+
+ALTER ROLE db_datawriter
+  ADD MEMBER [schwammystreamsweb-svc-systest] 
+END
